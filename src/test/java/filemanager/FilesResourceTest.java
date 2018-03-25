b469cb6816a9bcde84ab133d8f0ffcaed064afd7
sekia <sekia556.nospam@gmail.com>
2018-03-25 17:35:03 +0900

    Refactor test code

--- src/test/java/filemanager/FilesResourceTest.java
+++ src/test/java/filemanager/FilesResourceTest.java
@@ -8,9 +8,7 @@ import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -26,153 +24,55 @@ public class FilesResourceTest {
 
   private static final String baseURL = "http://localhost:8080/filemanager/rest/files";
 
-
-  // public void shouldGetAlreadyCreatedSchedule() throws Exception {
-  // final URL url = new URL(base, "schedule/" + scheduleId);
-  // final WebTarget target = ClientBuilder.newClient().target(url.toExternalForm());
-  // final Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
-  // assertEquals(200, response.getStatus());
-  // final JsonObject jsonObject = readJsonContent(response);
-  // assertEquals(scheduleId, jsonObject.getString("id"));
-  // assertEquals(TEST_SESSION, jsonObject.getString("sessionId"));
-  // assertEquals(TEST_VENUE, jsonObject.getString("venue"));
-  // }
-  //
-  //
-  // public void shouldGetScheduledSessionsForVenue() throws Exception {
-  // final String microprofile = String.valueOf(20);
-  // final String javaeeNext = String.valueOf(40);
-  // final String payaraMicro = String.valueOf(60);
-  //
-  // createScheduledSession(microprofile, "Hilton", String.valueOf(500), LocalDate.now(),
-  // LocalTime.now());
-  // createScheduledSession(javaeeNext, "Moscone", String.valueOf(600), LocalDate.now(),
-  // LocalTime.now());
-  // createScheduledSession(payaraMicro, "Hilton", String.valueOf(500), LocalDate.now(),
-  // LocalTime.now());
-  //
-  // final URL url = new URL(base, "schedule/venue/500");
-  // final WebTarget target = ClientBuilder.newClient().target(url.toExternalForm());
-  // final Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
-  // assertEquals(200, response.getStatus());
-  // final JsonArray jsonArray = readJsonArray(response);
-  // assertEquals(2, jsonArray.size());
-  // }
-  //
-  // @Test
-  // public void shouldGetActiveScheduledSessions() throws Exception {
-  // final String webServices = String.valueOf(100);
-  // final String designPatterns = String.valueOf(120);
-  // final String java = String.valueOf(140);
-  //
-  // createScheduledSession(webServices, "Moscone", String.valueOf(600), LocalDate.of(1995, 9, 20),
-  // LocalTime.of(16, 0));
-  // createScheduledSession(designPatterns, "Moscone", String.valueOf(600),
-  // LocalDate.of(1995, 9, 20), LocalTime.of(17, 0));
-  // createScheduledSession(java, "Moscone", String.valueOf(600), LocalDate.of(1995, 9, 21),
-  // LocalTime.of(16, 0));
-  //
-  // final URL url = new URL(base, "schedule/active/" + LocalDateTime.of(1995, 9, 20, 17, 34, 29));
-  // final WebTarget target = ClientBuilder.newClient().target(url.toExternalForm());
-  // final Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
-  // assertEquals(200, response.getStatus());
-  // final JsonArray jsonArray = readJsonArray(response);
-  // assertEquals(1, jsonArray.size());
-  // }
-  //
-  // @Test
-  // public void shouldGetScheduledSessionsByDate() throws Exception {
-  // final URL url = new URL(base, "schedule/all/" + LocalDate.of(1995, 9, 20));
-  // final WebTarget target = ClientBuilder.newClient().target(url.toExternalForm());
-  // final Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
-  // assertEquals(200, response.getStatus());
-  // final JsonArray jsonArray = readJsonArray(response);
-  // assertEquals(2, jsonArray.size());
-  // }
-  //
-  // @Test
-  // public void shouldRemoveSchedule() throws Exception {
-  // final String removeMe = String.valueOf(200);
-  // final Response createResponse = createScheduledSession(removeMe, "Far far away",
-  // String.valueOf(666), LocalDate.now(), LocalTime.now());
-  // final String removeId = getScheduleId(createResponse);
-  //
-  // final URL url = new URL(base, "schedule/" + removeId);
-  // final WebTarget target = ClientBuilder.newClient().target(url.toExternalForm());
-  // final Response deleteResponse = target.request(MediaType.APPLICATION_JSON_TYPE).delete();
-  // assertEquals(204, deleteResponse.getStatus());
-  //
-  // final URL checkUrl = new URL(base, "schedule/" + removeId);
-  // final WebTarget checkTarget = ClientBuilder.newClient().target(checkUrl.toExternalForm());
-  // final Response checkResponse = checkTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
-  // assertEquals(404, checkResponse.getStatus());
-  // }
-
-  private static JsonObject readJsonContent(final Response response) {
-    final JsonReader jsonReader = readJsonStringFromResponse(response);
-    return jsonReader.readObject();
-  }
-
-  private static JsonArray readJsonArray(final Response response) {
-    final JsonReader jsonReader = readJsonStringFromResponse(response);
-    return jsonReader.readArray();
-  }
-
-  private static JsonReader readJsonStringFromResponse(final Response response) {
-    final String competitionJson = response.readEntity(String.class);
-    final StringReader stringReader = new StringReader(competitionJson);
-    return Json.createReader(stringReader);
-  }
-
-  private static JsonObject getJsonObject(Response response) {
-    String jsonStr = response.readEntity(String.class);
-    StringReader reader = new StringReader(jsonStr);
-    return Json.createReader(reader).readObject();
-  }
-
-
   @Test
   public void shouldCreateReadDeleteFile() throws IOException {
     final String filename = "test.txt";
     File file = createTestFile(filename);
 
-    //prepare: the test file doesn't exist
+    // prepare: the test file must not exist
     deleteFile(filename);
 
-    //register the file
+    // register the file
     Response res1 = postFile(file);
     System.out.println("res1=" + res1);
+    JsonObject jsonObj1 = getJsonObject(res1);
+    System.out.println("res1 json=" + jsonObj1);
     assertEquals(200, res1.getStatus());
+    assertEquals(filename, jsonObj1.getString("id"));
 
-    //confirm the file is in a file list
+    // the file name must be in a file list
     Response res2 = getFiles();
     System.out.println("res2=" + res2);
     assertEquals(200, res2.getStatus());
-    JsonObject jsonObj1 = getJsonObject(res2);
-    System.out.println("res2 json=" + jsonObj1);
-    assertEquals(true, jsonObj1.containsKey(filename));
-    // assertEquals(filename, jsonObject.getString("id"));
+    JsonObject jsonObj2 = getJsonObject(res2);
+    System.out.println("res2 json=" + jsonObj2);
+    assertEquals(true, jsonObj2.containsKey(filename));
 
+    // a file get from the server must be the same as the registered
     Response res3 = getFile(filename);
     System.out.println("res3=" + res3);
     assertEquals(200, res3.getStatus());
-    System.out.println(res3.readEntity(File.class));
-    // java.nio.file.Files.isSameFile(path, path2)
+    File tmpFile3 = res3.readEntity(File.class);
+    System.out.println(tmpFile3);
+    assertEquals(file.length(), tmpFile3.length());
 
 
-    
+    // a file which doesn't exist must not be got
     Response res4 = getFile("no-exists.txt");
     System.out.println("res4=" + res4);
     assertEquals(404, res4.getStatus());
 
+    // a file must be deleted
     Response res5 = deleteFile(filename);
     System.out.println("res5=" + res5);
     assertEquals(204, res5.getStatus());
 
+    // a file which doesn't exist must be deleted
     Response res6 = deleteFile(filename);
     System.out.println("res6=" + res6);
     assertEquals(404, res6.getStatus());
 
+    // a file list doen't contain the file which has been deleted
     Response res7 = getFiles();
     System.out.println("res7=" + res7);
     assertEquals(200, res7.getStatus());
@@ -196,6 +96,14 @@ public class FilesResourceTest {
       throw e;
     }
   }
+
+
+  private static JsonObject getJsonObject(Response response) {
+    String jsonStr = response.readEntity(String.class);
+    StringReader reader = new StringReader(jsonStr);
+    return Json.createReader(reader).readObject();
+  }
+
 
 
   private Response getFiles() throws MalformedURLException {
